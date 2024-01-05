578d669e101a4a02f8dfa6b12c7daf15ece3691b
Mittaanjali <mitta.anjali@mphasis.com>
2024-01-05 17:36:06 +0530

    ClassLibrary

---
+++ ClassLibrary/Arithmetic.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassLibrary
+{
+    public class Arithmetic
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        public int Sub()
+        {
+            int res;
+            res = num1 - num2;
+            return res;
+        }
+        public int Multi()
+        {
+            int res;
+            res = num1 * num2;
+            return res;
+        }
+        public int Div()
+        {
+            int res;
+            res = num1 / num2;
+            return res;
+        }
+
+    }
+}
